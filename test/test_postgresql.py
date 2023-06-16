d0ee77aa0a57fc5cc1f3397e407ecb095fb9527c
frey808 <kaifrazier413@gmail.com>
2023-06-15 23:48:56 -1000

    setup db and tests

---
+++ test/test_postgresql.py
@@ -0,0 +1,11 @@
+import unittest
+from src.swen344_db_utils import *
+
+class TestPostgreSQL(unittest.TestCase):
+
+    def test_can_connect(self):
+        result = exec_get_one('SELECT VERSION()')
+        self.assertTrue(result[0].startswith('PostgreSQL'))
+
+if __name__ == '__main__':
+    unittest.main()
