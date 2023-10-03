a3a2ad45fd0de235457a6ea5b5f9cbf3e0da8663
frey808 <kaifrazier413@gmail.com>
2023-10-02 20:26:16 -0400

    reactstrap install

--- skate/src/index.js
+++ skate/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import Skate from './Skate';
+import Skate from 'Skate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
